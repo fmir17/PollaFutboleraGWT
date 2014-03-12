0d194f191e476d5a309fb35453ce86389dac8193
fmir17 <f.mir17@gmail.com>
2014-03-11 21:52:21 -0500

    proyecto inicial

---
+++ src/co/edu/udea/iw/client/GWTPollaFutbolera.java
@@ -0,0 +1,17 @@
+package co.edu.udea.iw.client;
+
+
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.i18n.client.Dictionary;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * Entry point classes define <code>onModuleLoad()</code>.
+ */
+public class GWTPollaFutbolera implements EntryPoint {
+	
+		
+	public void onModuleLoad() {}
+	
+}
